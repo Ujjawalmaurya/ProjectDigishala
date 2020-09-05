49adcf312681ad2aac5703c907e1a4caebbf96f6
suneater <suneater@pop-os.localdomain>
2020-09-05 22:13:59 +0530

    About us changed

--- lib/aboutUs/aboutUs.dart
+++ lib/aboutUs/aboutUs.dart
@@ -1,12 +1,7 @@
-import 'package:digishala/aboutUs/admission.dart';
-import 'package:digishala/constants.dart';
-import 'package:digishala/homepage.dart';
-import 'package:digishala/student/stuLoginPage.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AboutUs extends StatefulWidget {
-  static const String id = 'aboutUs';
+  static const String id = 'AboutUs';
   @override
   _AboutUsState createState() => _AboutUsState();
 }
@@ -16,89 +11,16 @@ class _AboutUsState extends State<AboutUs> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 10.0,
-        actions: <Widget>[
-          Image(
-            image: AssetImage('assets/mascot.png'),
-          ),
-        ],
-        backgroundColor: kThemeColor,
-        title: Text("About Us"),
-      ),
-      ////=======
-      ///Drawer==
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-                decoration: BoxDecoration(color: kThemeColor),
-                child: Row(children: [
-                  CircleAvatar(
-                      radius: MediaQuery.of(context).size.width * 0.15,
-                      child: Image(
-                        image: AssetImage('assets/mascot.png'),
-                      )),
-                  Column(children: [
-                    Text('Navodaya',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            letterSpacing: 1.5,
-                            fontWeight: FontWeight.w800,
-                            color: Colors.amberAccent)),
-                    Text('Children\'s',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            letterSpacing: 1.5,
-                            fontWeight: FontWeight.w800,
-                            color: Colors.amberAccent)),
-                    Text('Academy',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            letterSpacing: 1.5,
-                            fontWeight: FontWeight.w800,
-                            color: Colors.amberAccent)),
-                    Text('DigiShala',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 15.0,
-                            letterSpacing: 7,
-                            fontWeight: FontWeight.w300,
-                            color: Colors.white))
-                  ])
-                ])),
-            ListTile(
-              title: Text("About us"),
-              leading: Icon(Icons.account_balance, color: kThemeColor),
-              onTap: () => Navigator.of(context).pop(),
-            ),
-            Divider(color: kThemeColor),
-            ListTile(
-              title: Text("Home"),
-              leading: FaIcon(FontAwesomeIcons.home, color: kThemeColor),
-              onTap: () => Navigator.of(context)
-                  .pushNamedAndRemoveUntil(HomePage.id, (route) => false),
-            ),
-            ListTile(
-              title: Text("Login"),
-              leading: FaIcon(FontAwesomeIcons.arrowAltCircleRight,
-                  color: kThemeColor),
-              onTap: () => Navigator.of(context)
-                  .pushReplacementNamed(StudentLoginScreen.id),
-            ),
-            Divider(color: kThemeColor),
-            ListTile(
-              title: Text("Privacy Policy"),
-            ),
-          ],
-        ),
+        title: Text('About US'),
+        backgroundColor: Colors.purple,
       ),
       body: Container(
-        decoration: kContainerThemeDecoration,
-        width: double.infinity,
-        height: double.infinity,
+        height: MediaQuery.of(context).size.height / 1,
+        width: MediaQuery.of(context).size.width / 1,
+        child: Center(
+          child:
+              Text('Not final Build Credits will be shown after final build'),
+        ),
       ),
     );
   }
